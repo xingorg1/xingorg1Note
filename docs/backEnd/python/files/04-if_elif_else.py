0ec61345ffd1c9b584539493d3f72d68c925ac2c
xingorg1 <xing.org1@outlook.com>
2020-01-12 23:58:58 +0800

    [#+]python的if语句

--- docs/backEnd/python/files/04-if_elif_else.py
+++ docs/backEnd/python/files/04-if_elif_else.py
@@ -7,3 +7,47 @@ elif number=='3':
   print('3')
 else:
   print('heihei')
+
+number2=6
+# 条件：如果number数量小于6，打印“我不爱你了”
+if number2 < 6:
+    print('我不爱你了222')
+
+print('我没有错缩进，不是if的语句块，不受条件限制，我都要打印。')
+# 我没有错缩进，不是if的语句块，不受条件限制，我都要打印。
+
+bool = False
+if bool:
+  print('我爱你')
+else:
+  print('我不爱你了')
+
+print('我没有错缩进，不是if的语句块，不受条件限制，我都要打印。')
+
+score = 74
+if score > 99:
+  print('优秀')
+elif score > 85:
+  print('良好')
+elif score > 66:
+  print('及格')
+else:
+  print('好意思吗？！')
+
+
+goal = 94
+if goal > 99:
+  if goal < 105:
+    print('天才')
+  else:
+    print('人间精品')
+elif goal > 85:
+  if goal > 90:
+    print('优秀')
+  else:
+    print('良好')
+  print('是个人才！')
+elif goal > 66:
+  print('及格')
+else:
+  print('好意思吗？！')
--- docs/webNote/js/README.md
+++ docs/webNote/js/README.md
@@ -1,8 +1,30 @@
 ---
-title: JS模块
-description: JS模块
+title: JS模块 知识点合集
+description: JS模块 知识点合集 knowledge Point
 ---
 
-# JS 模块
+# knowledge Point
+知识点啊！朋友们。知识点！画起来，要考的。
+## resize只能绑定一个？
+对的，用下边这种写法，一个页面中，resize只能绑定一个。
 
-平时用到的一些常见 JS 模块记录笔记
+下边的代码，就永远只会打印出“2”，而没有“1”。
+```js
+// resize只能绑定、执行一个。此时只执行后边那个。
+window.onresize = function(){
+    console.log("1")
+}
+window.onresize = function(){
+    console.log("2")
+}
+```
+但是用下边这种写法，俩都可以执行了。
+```js
+// addEventListener绑定，就可以两次都执行
+window.addEventListener("resize", function(){
+    console.log("3")
+});
+window.addEventListener("resize", function(){
+    console.log("4")
+});
+```
