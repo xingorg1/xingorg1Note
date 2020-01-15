2f1e05457278ea81cd7e264280c5815a72503ea4
xingorg1 <xing.org1@outlook.com>
2020-01-15 13:23:12 +0800

    [+]自动合并

---
+++ mergeMaster.sh
@@ -0,0 +1,16 @@
+# develop 合并 master
+# 启动：bash mergeMaster.sh
+# 确保脚本抛出遇到的错误
+set -e
+
+git pull
+
+git merge master
+
+git add .
+
+git commit -m "[&]merge master to develop"
+
+git pull
+
+git push
