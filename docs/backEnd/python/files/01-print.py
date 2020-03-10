57b95c0e8eb8bbd35df030467850e88592596af2
xingorg1 <xing.org1@outlook.com>
2020-03-10 22:58:22 +0800

    [#]print打印修改控制台颜色

--- docs/backEnd/python/files/01-print.py
+++ docs/backEnd/python/files/01-print.py
@@ -3,3 +3,7 @@ print('小石头', 'xing.org1^', 'gjf', sep = '、')
 # sep控制多个值之间的分隔符，默认是空格
 print('小石头', 'xing.org1^', 'gjf', sep = '、', end = '... \n')
 # end控制打印结果的结尾，默认是换行)
+
+# 设置终端字体颜色，参照https://www.cnblogs.com/yinjia/p/5559702.html
+# print('\033[1;31;40m')
+# print("攻击\t", 10, "\t", "\033[1;32m\033[1:0m " * (10 // 10))
